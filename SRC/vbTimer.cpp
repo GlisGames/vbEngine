1c0d6437b3a1529cd3b0fafbdac252040a9e0a92
anhnv1703 <anhnv1703@gmail.com>
2021-12-22 09:14:41 +0700

    Added getters & setters, added callback for vbTimer

--- SRC/vbTimer.cpp
+++ SRC/vbTimer.cpp
@@ -1,26 +1,61 @@
 #include "vbEngine.h"
 
-void vbTimer::init(double _duration) {
+// INIT
+void vbTimer::init(double _duration, timer_callback callback) {
 	this->isAlive = TRUE;
 	this->initTime = GetTime();
 	this->duration = _duration;
+	this->callbackEnd = callback;
 }
-
-vbTimer::vbTimer(double _duration) {
-	this->init(_duration);
+// CTOR
+vbTimer::vbTimer(double _duration, timer_callback callback) {
+	this->init(_duration, callback);
 }
-
+// UPDATE
 void vbTimer::update(){
-	if (this->getTimeAlive() > this->duration) {
-		printf("TIME'S UP");
-		this->reset();
+	if (this->getTimeAlive() >= this->duration) {
+		this->isTimesUp = TRUE;
+	}
+
+	if (this->isTimesUp) {
+		if (this->callbackEnd != NULL) {
+			this->callbackEnd();
+		}
+		if (this->endLambda != NULL) {
+			this->endLambda();
+		}
 	}
 }
-
+// GETTERS & SETTERS
+double vbTimer::getInitTime() {
+	return this->initTime;
+}
+double vbTimer::getDuration() {
+	return this->duration;
+}
+void vbTimer::setDuration(double _duration) {
+	this->duration = _duration;
+}
+// OTHERS
 void vbTimer::reset() {
 	this->initTime = GetTime();
+	this->isTimesUp = FALSE;
 }
-
 double vbTimer::getTimeAlive() {
 	return (GetTime() - this->initTime);
 }
+//template <class callable, class... arguments>
+//void vbTimer::later(bool async, callable&& f, arguments&&... args) {
+//	std::function<typename std::result_of<callable(arguments...)>::type()> task(std::bind(std::forward<callable>(f), std::forward<arguments>(args)...));
+//
+//    if (async) {
+//        std::thread([this->duration, task]() {
+//            std::this_thread::sleep_for(std::chrono::seconds(this->duration));
+//            task();
+//        }).detach();
+//    }
+//    else {
+//        std::this_thread::sleep_for(std::chrono::seconds(this->duration));
+//        task();
+//    }
+//}
--- SRC/vbTimer.h
+++ SRC/vbTimer.h
@@ -1,15 +1,32 @@
 #ifndef VBTIMER_H
 #define VBTIMER_H
+#include <functional>
 
+
+typedef void (*timer_callback) ();
 class vbTimer : public vbLivingObject {
 private:
 	double initTime = GetTime();
 	double duration = 0;
-	void init(double _duration);
+	bool isTimesUp = FALSE;
+	void init(double _duration, timer_callback callback);
 public:
-	vbTimer(double _duration = 1);
+	timer_callback callbackEnd = NULL;
+	std::function<void()> endLambda = NULL;
+	vbTimer* endLambdaSet(std::function<void()> f) {
+		this->endLambda = f;
+		return this;
+	}
+
+	vbTimer(double _duration = 1, timer_callback callback = NULL);
 	void update();
 	void reset();
 	double getTimeAlive();
+	double getInitTime();
+	double getDuration();
+	void setDuration(double _duration);
+
+	//template <class callable, class... arguments>
+	//void later(bool async, callable&& f, arguments&&... args); // TESTING Thread safe with callable 
 };
 #endif // !VBTIMER_H
