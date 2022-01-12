a2b893d8fb5e863d9ff58b0a9f731aa07b0f31fe
Laszlo Nguyen <anhnv1703@gmail.com>
2022-01-12 17:43:13 +0700

    Added callbackKill and killLambdaSet in vbTween. Renamed some vbTween functions

--- SRC/vbMultimedia/vbTween.cpp
+++ SRC/vbMultimedia/vbTween.cpp
@@ -4,9 +4,11 @@ vbTween::vbTween()
 
 vbTween::~vbTween()
 {
+	if (this->callbackKill)
+		this->callbackKill;
 }
 
-void vbTween::init(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+void vbTween::init(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->startP = Start_p;
 	this->stopP = Stop_p;
@@ -17,39 +19,69 @@ void vbTween::init(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop
 	this->repeatFor = repeatFor;
 	this->repeatSet = repeatFor;
 	this->callbackEnd = callback;
-	//this->endLambda = NULL;
+	this->callbackKill = callbackKill;
 }
-vbTween::vbTween(BYTE* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+FLOAT vbTween::doTween(FLOAT Start_p, FLOAT Stop_p, WORD currentStep, WORD TOTsteps)
+{
+	if (currentStep >= TOTsteps)
+		currentStep = TOTsteps;
+	return (Stop_p - Start_p) * ((FLOAT)currentStep / (FLOAT)TOTsteps) + Start_p;
+}
+FLOAT vbTween::doTween(FLOAT Start_p, FLOAT Stop_p, DWORD currentStep, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction)
+{
+	FLOAT Current_p;
+	FLOAT p;
+	if (loop == twOneShot || loop == twRepeatOnce)
+	{
+		if (currentStep >= TOTsteps)
+			currentStep = TOTsteps;
+	}
+	else
+	{
+		if (loop == twRepeat)
+			currentStep = (currentStep) % (TOTsteps);
+		else if (loop == twYoyo)
+		{
+			currentStep = ((currentStep % (TOTsteps * 2)) >= TOTsteps) ? ((TOTsteps - 1) - (currentStep % TOTsteps)) : currentStep;
+			currentStep %= TOTsteps;
+		}
+	}
+	p = (FLOAT)((double)currentStep / (TOTsteps - 1));
+	p = easingFunction(p);
+	Current_p = (Stop_p - Start_p) * p + Start_p;
+	return Current_p;
+}
+vbTween::vbTween(BYTE* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueBYTE = (value);
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween::vbTween(WORD* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+vbTween::vbTween(WORD* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueWORD = (value);
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween::vbTween(DWORD* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+vbTween::vbTween(DWORD* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueDWORD = (value);
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween::vbTween(FLOAT* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+vbTween::vbTween(FLOAT* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueFLOAT = (value);
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween::vbTween(int* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+vbTween::vbTween(int* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueINT = (value);
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween::vbTween(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback)
+vbTween::vbTween(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill)
 {
 	this->valueDWORD = &this->valueTIMER;
-	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback);
+	init(Start_p, Stop_p, TOTsteps, loop, easingFunction, repeatFor, callback, callbackKill);
 }
-vbTween* vbTween::Enable()
+vbTween* vbTween::Play()
 {
 	this->enabled = TRUE;
 	return this;
@@ -61,10 +93,10 @@ vbTween* vbTween::Pause()
 	return this;
 }
 
-vbTween* vbTween::Play()
+vbTween* vbTween::Restart()
 {
 	this->Reset();
-	this->Enable();
+	this->Play();
 	return this;
 }
 
@@ -75,13 +107,9 @@ vbTween* vbTween::Stop()
 	return this;
 }
 
-/// <summary>
-/// Complete the execution of the tween to the end, it is transformed to a oneshot
-/// </summary>
+// Complete the execution of the tween to the end, it is transformed to a oneshot
 void vbTween::Finish()
 {
-	//this->currStep = this->totStep;
-	//this->repeatFor = 0;
 	this->repeat = twOneShot;
 	this->currStep = this->currStep % this->totStep;
 }
@@ -90,8 +118,6 @@ vbTween* vbTween::Reset()
 {
 	this->currStep = 0;
 	this->repeatFor = this->repeatSet;
-	//if(this->repeatFor != -1)
-	//	this->repeatFor = 0;
 	return this;
 }
 
@@ -123,4 +149,92 @@ BOOL vbTween::isFinished()
 	else if (this->currStep == 0 && this->totStep == 0)
 		return TRUE;
 	return ((this->currStep % this->totStep)==0);
+}
+
+BOOL vbTween::isTerminated()
+{
+	return (this->repeatFor == 0);
+}
+
+
+// TWEENMAP
+vbTween* vbTweenMap::addtween(const char* name, vbTween tw)
+{
+	this->insert(std::pair<const char*, vbTween>(name, tw));
+	return &this->operator[](name);
+}
+vbTween* vbTweenMap::addtimer(const char* name, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int numRepeats, tween_callback callback)
+{
+	//FIXME solve conflicting names inside the map
+	this->insert(std::pair<const char*, vbTween>(name, vbTween(0, (FLOAT)TOTsteps, TOTsteps, loop, easingFunction, numRepeats, callback)));
+	return &this->operator[](name);
+}
+vbTween* vbTweenMap::tweenGet(const char* name)
+{
+	if (this->find(name) == this->end())
+		return NULL;
+	else
+		return &this->operator[](name);
+}
+void vbTweenMap::killTween(const char* name)
+{
+	if (this->find(name) != this->end())
+		this->erase(name);
+}
+void vbTweenMap::killAll()
+{
+	if (this->size() == 0)
+		return;
+	vbTweenMap::iterator itw = this->begin();
+	while (itw != this->end())
+	{
+		vbTweenMap::iterator toErase = itw;
+		itw++;
+		this->erase(toErase);
+	}
+}
+void vbTweenMap::stopAll()
+{
+	if (this->size() == 0)
+		return;
+	vbTweenMap::iterator itw = this->begin();
+	while (itw != this->end())
+	{
+		itw->second.Stop();
+		itw++;
+	}
+}
+void vbTweenMap::stepAll()
+{
+	if (this->size() == 0)
+		return;
+
+	std::vector<vbTween*> nextList;
+	vbTweenMap::iterator itw = this->begin();
+	while (itw != this->end())
+	{
+		itw->second.Step();
+		if (itw->second.isFinished() && itw->second.repeatFor > 0)
+			itw->second.repeatFor--;
+
+		if (itw->second.isFinished())
+		{
+			if (itw->second.callbackEnd != NULL)
+				itw->second.callbackEnd();
+
+			if (itw->second.next && itw->second.repeatFor == 0 && itw->second.repeatSet != 0)
+				nextList.push_back(itw->second.next);
+			if ((itw->second.repeat == twOneShot) || (itw->second.repeatFor == 0))
+				itw = this->erase(itw);
+		}
+		if (itw != this->end())
+			itw++;
+	}
+
+	for (WORD i = 0; i < nextList.size(); i++)
+	{
+		nextList[i]->enabled = TRUE;
+		//this->addtween("next", *nextList[i]);
+		//nextList[i]->currStep = 1;
+	}
 }
--- SRC/vbMultimedia/vbTween.h
+++ SRC/vbMultimedia/vbTween.h
@@ -12,81 +12,58 @@ enum tweenRepeat
 	twYoyo = 2, //Repeats from start to end then from end to start
 	twRepeatOnce = 3 //Repeats from start to end only once
 };
-//typedef void (*tween_callback) ();
+
 typedef std::function<void()> tween_callback;
-class vbTween;
+
 class vbTween
 {
 private:
-	void init(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback);
+	void init(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction, int repeatFor, tween_callback callback, tween_callback callbackKill);
 public:
-	static FLOAT doTween(FLOAT Start_p, FLOAT Stop_p, WORD currentStep, WORD TOTsteps)
-	{
-		if (currentStep >= TOTsteps)
-			currentStep = TOTsteps;
-		return (Stop_p - Start_p) * ((FLOAT)currentStep / (FLOAT)TOTsteps) + Start_p;
-	}
-	static FLOAT doTween(FLOAT Start_p, FLOAT Stop_p, DWORD currentStep, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction)
-	{
-		FLOAT Current_p;
-		FLOAT p;
-		if (loop == twOneShot || loop == twRepeatOnce)
-		{
-			if (currentStep >= TOTsteps)
-				currentStep = TOTsteps;
-		}
-		else
-		{
-			if (loop == twRepeat)
-				currentStep = (currentStep) % (TOTsteps);
-			else if (loop == twYoyo)
-			{
-				currentStep = ((currentStep % (TOTsteps * 2)) >= TOTsteps) ? ((TOTsteps - 1) - (currentStep % TOTsteps)) : currentStep;
-				currentStep %= TOTsteps;
-			}
-		}
-		p = (FLOAT)((double)currentStep / (TOTsteps-1));
-		p = easingFunction(p);
-		Current_p = (Stop_p - Start_p) * p + Start_p;
-		return Current_p;
-	}
+	static FLOAT doTween(FLOAT Start_p, FLOAT Stop_p, WORD currentStep, WORD TOTsteps);
+	static FLOAT doTween(FLOAT Start_p, FLOAT Stop_p, DWORD currentStep, DWORD TOTsteps, tweenRepeat loop, EasingFunction easingFunction);
 	BOOL enabled = TRUE;
 	DWORD currStep = 0;
 	DWORD totStep = 1;
 	FLOAT startP = 0;
 	FLOAT stopP = 0;
-	int repeatFor = -1;
-	int repeatSet = -1; //initial repeat value that has been set
+	int repeatFor = 0;
+	int repeatSet = 0; //initial repeat value that has been set
 	BYTE *valueBYTE = NULL;
 	WORD *valueWORD = NULL;
 	DWORD *valueDWORD = NULL;
 	FLOAT *valueFLOAT = NULL;
 	DWORD valueTIMER = NULL;
 	tween_callback callbackEnd = NULL;
+	tween_callback callbackKill = NULL;
 	int *valueINT = NULL;
 	tweenRepeat repeat = twYoyo;
 	EasingFunction easingF = LinearInterpolation;
-	//tween_callback callbackEnd = NULL;
-	vbTween* endLambdaSet(std::function<void()> f) {
+	vbTween* endLambdaSet(tween_callback f) {
 		this->callbackEnd = f;
+		return this;
+	}
+	vbTween* killLambdaSet(tween_callback f) {
+		this->callbackKill = f;
 		return this;
 	}
 	vbTween();
 	~vbTween();
-	vbTween(BYTE *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation,  int repeatFor = -1, tween_callback callback = NULL);
-	vbTween(WORD *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation,  int repeatFor = -1, tween_callback callback = NULL);
-	vbTween(DWORD *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor =  -1, tween_callback callback = NULL);
-	vbTween(FLOAT *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor =  -1, tween_callback callback = NULL);
-	vbTween(int *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor =  -1, tween_callback callback = NULL);
-	vbTween(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twOneShot, EasingFunction easingFunction = LinearInterpolation, int repeatFor =  -1, tween_callback callback = NULL);
-	vbTween* Enable();
+	vbTween(BYTE *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation,  int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween(WORD *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation,  int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween(DWORD *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween(FLOAT *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween(int *value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween(FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twOneShot, EasingFunction easingFunction = LinearInterpolation, int repeatFor = 0, tween_callback callback = NULL, tween_callback callbackKill = NULL);
+	vbTween* Play();
 	vbTween* Stop();
 	vbTween* Pause();
-	vbTween* Play();
+	vbTween* Restart();
 	vbTween* Reset();
 	void Step();
 	void Finish();
 	BOOL isFinished();
+	BOOL isTerminated();
 	vbTween* next = NULL;
 	vbTween* addNext(vbTween* n)
 	{
@@ -99,102 +76,20 @@ public:
 class vbTweenMap : public std::map<const char*, vbTween>
 {
 public:
-	void killTween(const char* name)
-	{
-		if (this->find(name) != this->end())
-			this->erase(name);
-	}
-
-	void killAll()
-	{
-		if (this->size() == 0)
-			return;
-		vbTweenMap::iterator itw = this->begin();
-		while (itw != this->end())
-		{
-			vbTweenMap::iterator toErase = itw;
-			itw++;
-			this->erase(toErase);
-		}
-	}
-
 	template <typename T>
-	vbTween* addtween(const char* name, T* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int numRepeats = -1, tween_callback callback = NULL)
+	vbTween* addtween(const char* name, T* value, FLOAT Start_p, FLOAT Stop_p, DWORD TOTsteps, tweenRepeat loop = twYoyo, EasingFunction easingFunction = LinearInterpolation, int numRepeats = 0, tween_callback callback = NULL)
 	{
 		//FIXME solve conflicting names inside the map
 		this->insert(std::pair<const char*, vbTween>(name, vbTween(value, Start_p, Stop_p, TOTsteps, loop, easingFunction, numRepeats, callback)));
 		return &this->operator[](name);
 	};
-
-	vbTween* addtimer(const char* name, DWORD TOTsteps, tweenRepeat loop = twOneShot, EasingFunction easingFunction = LinearInterpolation, int numRepeats = -1, tween_callback callback = NULL)
-	{
-		//FIXME solve conflicting names inside the map
-		this->insert(std::pair<const char*, vbTween>(name, vbTween(0, (FLOAT)TOTsteps, TOTsteps, loop, easingFunction, numRepeats, callback)));
-		return &this->operator[](name);
-	};
-	vbTween *addtween(const char* name, vbTween tw)
-	{
-		this->insert(std::pair<const char*, vbTween>(name, tw));
-		return &this->operator[](name);
-	};
-
-	vbTween* tweenGet(const char* name)
-	{
-		if (this->find(name) == this->end())
-			return NULL;
-		else
-			return &this->operator[](name);
-	}
-
-	void stopAll()
-	{
-		if (this->size() == 0)
-			return;
-		vbTweenMap::iterator itw = this->begin();
-		while (itw != this->end())
-		{
-			itw->second.Stop();
-			itw++;
-		}
-	}
-
-	void stepAll()
-	{
-		if (this->size() == 0)
-			return;
-
-		std::vector<vbTween*> nextList;
-		vbTweenMap::iterator itw = this->begin();
-		while (itw != this->end())
-		{
-			itw->second.Step();
-			if (itw->second.isFinished() && itw->second.repeatFor != -1)
-				itw->second.repeatFor--;
-
-			if (itw->second.isFinished())
-			{
-				if (itw->second.callbackEnd != NULL)
-					itw->second.callbackEnd();
-				//if (itw->second.endLambda != NULL)
-				//	itw->second.endLambda();
-
-				if (itw->second.next)
-					nextList.push_back(itw->second.next);
-				if ((itw->second.repeat == twOneShot) || (itw->second.repeatFor == 0))
-					itw = this->erase(itw);
-
-			}
-			if (itw != this->end())
-				itw++;
-		}
-
-		for (WORD i = 0; i < nextList.size(); i++)
-		{
-			nextList[i]->enabled = TRUE;
-			//this->addtween("next", *nextList[i]);
-			//nextList[i]->currStep = 1;
-		}
-	}
+	vbTween* addtween(const char* name, vbTween tw);
+	vbTween* addtimer(const char* name, DWORD TOTsteps, tweenRepeat loop = twOneShot, EasingFunction easingFunction = LinearInterpolation, int numRepeats = 0, tween_callback callback = NULL);
+	vbTween* tweenGet(const char* name);
+	void killTween(const char* name);
+	void killAll();
+	void stopAll();
+	void stepAll();
 };
 
 //typedef std::map<const char *, vbTween> vbTweenMap;
