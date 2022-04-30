9621c8e5354ef41fac34d955d709eeccf64082b3
Michele Tampellini <mic.tampellini@gmail.com>
2022-04-30 14:39:23 +0700

    update online panic message

--- SRC/vbErrors.cpp
+++ SRC/vbErrors.cpp
@@ -2,20 +2,13 @@
 
 void PANIC(const char* message)
 {
-#ifdef PLATFORM_RASPBERRY
 	while (true)
 	{
 		WaitTime(100);
 		BeginDrawing();
 		ClearBackground(RED);
-		DrawText(TextFormat("ERROR"), 50, 0, 40, BLACK);
+		DrawText(TextFormat("ERROR, please refresh (F5) the page."), 50, 0, 40, BLACK);
 		DrawText(message, 50, 50, 40, BLACK);
-		//DrawText(TextFormat("READ: % d", digitalRead(7)), 0, 50, 40, BLACK);
-		//vbRender_printWorld(&GAME.mainGame);
 		EndDrawing();
 	}
-#else
-	TraceLog(LOG_ERROR, "\n PANIC: %s", message);
-	exit(1);
-#endif // PLATFORM_RASPBERRY
 }
