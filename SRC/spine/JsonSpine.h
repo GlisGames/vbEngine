c9ee78a142911d2481e4fba98b2d6cde2583d778
Chung Pham <phamanhchung559@gmail.com>
2022-06-16 15:42:18 +0700

    Compile but no color on objects

--- SRC/spine/JsonSpine.h
+++ SRC/spine/JsonSpine.h
@@ -1,16 +1,16 @@
 /*
  Copyright (c) 2009 Dave Gamble
- 
+
  Permission is hereby granted, dispose of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,6 +24,10 @@
 
 #ifndef SPINE_JSON_H_
 #define SPINE_JSON_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Json Types: */
 #define Json_False 0
@@ -71,5 +75,9 @@ int Json_getInt (Json* json, const char* name, int defaultValue);
 
 /* For analysing failed parses. This returns a pointer to the parse error. You'll probably need to look a few chars back to make sense of it. Defined when Json_create() returns 0. 0 when Json_create() succeeds. */
 const char* Json_getError (void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* SPINE_JSON_H_ */
